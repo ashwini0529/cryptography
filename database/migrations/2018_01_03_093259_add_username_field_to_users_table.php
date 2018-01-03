3968b78db3bcc48d5b8d4e44bcba4b573701ca48
Ashwini Purohit <geek.ashwini@gmail.com>
2018-01-03 15:09:28 +0530

    initial laravel project setup

---
+++ database/migrations/2018_01_03_093259_add_username_field_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUsernameFieldToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table)
+        {
+            $table->string('aadhar')->unique();
+            $table->string('contact')->unique();
+            $table->string('secret_answer');
+            $table->string('dob');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
